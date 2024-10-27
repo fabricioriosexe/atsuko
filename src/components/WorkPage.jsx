28d5f0d92ee73a958fa027603eddda4607999607
Dani Ortiz <danielorrami@hotmail.com>
2024-10-26 23:34:16 -0300

    Update WorkPage.jsx

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -50,7 +50,7 @@ function WorkPage() {
             <header className="flex justify-between items-center py-4 px-4 ">
                 <Link to="/">
                     <img
-                        src="/img/Atsuko.png"
+                        src="/img/Atsuko2.png"
                         alt="Atsuko Logo"
                         className="w-24 md:w-28 lg:w-32 xl:w-36" // Ajusta los tamaños aquí
                     />
